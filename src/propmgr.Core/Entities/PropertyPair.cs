eff194ea41dc17fbd33663f8c36c9ef6c95dea52
petrspelos <petr.spelos@gmail.com>
2019-07-23 17:11:26 +0200

    Add PropertyPair
    Add the PropertyPair entity class.

---
+++ src/propmgr.Core/Entities/PropertyPair.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace propmgr.Core.Entities
+{
+    public class PropertyPair
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+        public bool IsComment() => throw new NotImplementedException();
+
+        public bool IsEmptyLine() => throw new NotImplementedException();
+    }
+}
