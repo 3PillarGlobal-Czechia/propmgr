bc3b57e0ae8b4db871c82ab8d085c3a7707f5471
petrspelos <petr.spelos@gmail.com>
2019-07-28 13:20:42 +0200

    Implement strip empty lines command

---
+++ src/propmgr.Core/Commands/StripEmptyLinesCommand.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace propmgr.Core.Commands
+{
+    public class StripEmptyLinesCommand : ICommand
+    {
+        private readonly IPropertiesFile _file;
+
+        public StripEmptyLinesCommand(IPropertiesFile file)
+        {
+            _file = file;
+        }
+
+        public void Execute()
+        {
+            if (_file is null)
+                return;
+
+            RemoveEmptyLines();
+        }
+
+        private void RemoveEmptyLines()
+        {
+            var p = _file.GetProperties().Where(prop => !prop.IsEmptyLine());
+            _file.SetProperties(p);
+        }
+    }
+}
